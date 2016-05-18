deaf99640236419cdddef48256ceb84f8f59fdac
virtmerlin <merlin@sense.net>
2016-05-18 01:09:28 -0500

    INIT

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
 
+set -e
+
+echo "Getting unzip, git, & npm packages..."
 apt-get -y install git unzip
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+apt-get -y install git unzip nodejs
+
+echo "Installing CF cli..."
+dpkg -i cf-cli-installer*.deb
+
+echo "Installing UAAC cli..."
+gem install cf-uaac
+
+echo "Installing Azure cli..."
+npm install azure-cli -g
+
